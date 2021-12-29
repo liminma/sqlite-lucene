91642b25756e7a0d216b34d6db0925562bbbd291
liminma <limin.ma@ontariotechu.net>
2021-12-28 19:43:08 -0500

    add unit test cases for building index

--- src/test/kotlin/IndexingKtTest.kt
+++ src/test/kotlin/IndexingKtTest.kt
@@ -1,33 +1,20 @@
 import kotlinx.coroutines.runBlocking
 import org.apache.lucene.index.DirectoryReader
 import org.apache.lucene.store.FSDirectory
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
 import java.io.File
+import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.exists
 import kotlin.test.assertEquals
 
 const val INDEX_FOLDER = "lucene-index"
 
-internal class TextIndexWriterTest {
-
-    @BeforeEach
-    fun setUp() {
-    }
-
-    @AfterEach
-    fun tearDown() {
-    }
-
-    @Test
-    fun add() {
-    }
-}
-
 internal class IndexingKtTest {
-    val dataPath = Paths.get(javaClass.classLoader.getResource(SAMPLE_DB_FILE)!!.toURI())
-    val indexAbsLocation = dataPath.toFile().parent + "/" + INDEX_FOLDER
+    private val dataPath: Path = Paths.get(javaClass.classLoader.getResource(SAMPLE_DB_FILE)!!.toURI())
+    private val indexAbsLocation = dataPath.toFile().parent + "/" + INDEX_FOLDER
 
     @AfterEach
     fun tearDown() {
